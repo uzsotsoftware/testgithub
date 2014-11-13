a67aa4235fba65169ebdaefe4fb6b04a4419b20c
solkem <solomonkembo@gmail.com>
2014-11-13 10:16:30 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: software
+ *
+ * Created on November 13, 2014, 10:09 AM
+ */
+
+#include <cstdlib>
+#include<iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"testing github";
+    return 0;
+}
+
